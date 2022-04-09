7276cf8fa37bc0cea0cbb0edc67b7801476ae0a8
DZ <2119921883@qq.com>
2022-04-09 16:34:34 +0800

    修改图片上传路径

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -262,7 +262,8 @@ module.exports = (app) => {
   const upload = multer({ dest: __dirname + "../../../uploads" });
   app.post("/admin/api/upload", upload.single("file"), async (req, res) => {
     const file = req.file;
-    file.url = `http://localhost:3000/uploads/${file.filename}`;
+    // file.url = `http://localhost:3000/uploads/${file.filename}`;
+    file.url = `http://www.dz8545.xyz/uploads/${file.filename}`;
     res.send(file);
   });
 };
